1a86cd971f6709cc8e5b8d49c06de2d916c63750
Abdullah Yildirim <abh.yildirim@googlemail.com>
2018-01-18 20:51:22 +0100

    - App Namen geändert in LRS GAME - APP ICON/Logo gesetzt - Android Manifest kommentiert - Class Bilder kommentiert

--- app/src/main/java/com/example/hedenius01/Bilder.java
+++ app/src/main/java/com/example/hedenius01/Bilder.java
@@ -2,9 +2,12 @@ package com.example.hedenius01;
 /**
  * Created by Abdullah Yildirim on 27.11.2017.
  */
-//In dieser Klasse werden alle Images in einem Array gespeichert und von hier aus aufgerufen in den einzelnen ImageViews.
-public class Bilder {
+//In dieser Klasse werden alle Images in einem Array gespeichert.
+//Für jede Runde gibt es einen eigenen Array.
+//Von hier aus werden die einzelnen Bilder in den ImageViews gealden.
 
+public class Bilder {
+    //SPIEL MODUS BILDER
     //Runde 1
     //Set1 Bilder
     public static int[] images = new int []{
@@ -38,105 +41,103 @@ public class Bilder {
     public static int[] images5 = new int []{
             R.drawable.ob5notfiktiv1, R.drawable.ob5fiktiv1, R.drawable.ob5fiktiv2, R.drawable.ob5notfiktiv2,
             R.drawable.ob5notfiktiv3, R.drawable.ob5fiktiv3, R.drawable.ob5notfiktiv4, R.drawable.ob5fiktiv4
-
     };
 
     //Runde 6
-    //Set5 Bilder
+    //Set6 Bilder
     public static int[] images6 = new int []{
             R.drawable.ob6fiktiv1, R.drawable.ob6notfiktiv1, R.drawable.ob6fiktiv2, R.drawable.ob6notfiktiv2,
             R.drawable.ob6fiktiv3, R.drawable.ob6notfiktiv3, R.drawable.ob6fiktiv4, R.drawable.ob6notfiktiv4
-
     };
 
     //Runde 7
-    //Set5 Bilder
+    //Set7 Bilder
     public static int[] images7 = new int []{
             R.drawable.ob7notfiktiv1, R.drawable.ob7notfiktiv2, R.drawable.ob7fiktiv1, R.drawable.ob7notfiktiv3,
             R.drawable.ob7fiktiv2, R.drawable.ob7fiktiv3, R.drawable.ob7notfiktiv4, R.drawable.ob7fiktiv4
-
     };
 
     //Runde 8
-    //Set5 Bilder
+    //Set8 Bilder
     public static int[] images8 = new int []{
             R.drawable.ob8fiktiv1, R.drawable.ob8notfiktiv1, R.drawable.ob8notfiktiv2, R.drawable.ob8fiktiv2,
             R.drawable.ob8notfiktiv3, R.drawable.ob8notfiktiv4, R.drawable.ob8fiktiv3, R.drawable.ob8fiktiv4
-
     };
 
-    //Hier sind die Bilder für die beiden Trainingsrunden
+    //TRAINING MODUS BILDER
     //Training Runde 1
-    //SetTraining Bilder
+    //reale Elemente mit Hintergrund
     public static int[] training1images = new int []{
-            R.drawable.not_training1img1, R.drawable.set_training1img1, R.drawable.not_training1img2, R.drawable.set_training1img2
-
+            R.drawable.not_training1img1, R.drawable.set_training1img1,
+            R.drawable.not_training1img2, R.drawable.set_training1img2
     };
 
     //Training Runde 2
-    //SetTraining Bilder
+    //fiktive Elemente ohne Hintergrund
     public static int[] training2images = new int []{
-           R.drawable.set_training2img1 ,R.drawable.not_training2img1, R.drawable.not_training2img2, R.drawable.set_training2img2
-
+           R.drawable.set_training2img1 ,R.drawable.not_training2img1,
+            R.drawable.not_training2img2, R.drawable.set_training2img2
     };
 
-    //In diesen arrays werden die richtigen Antworten gespeichert und in den jeweiligen Runden aufgerufen und in die CSV geschrieben
-    //Antworten zu Runde1 real mit Hintergrund
+    //In diesen arrays werden die richtigen Antworten gespeichert.
+    // In den jeweiligen Runden werden diese in die CSV geschrieben.
+    // Am Ende werden die eigene Antworten mit diesen verglichen und in der CSV ausgewertet.
+    //Antworten zu Runde1 reale Elemente mit Hintergrund
     // 1 0 0 1 1 0 0 1
     public static boolean[] answers = new boolean[] {
             true, false, false, true , true , false, false, true
     };
 
-    //Antworten zu Runde2 real mit Hintergrund
+    //Antworten zu Runde2 reale Elemente mit Hintergrund
     // 0 0 1 1 0 0  1 1
     public static boolean[] answers2 = new boolean[] {
             false, false, true, true, false, false, true, true
     };
 
-    //Antworten zu Runde3 real mit Hintergrund
+    //Antworten zu Runde3 reale Elemente mit Hintergrund
     // 0 1 0 1 0 0 1 1
     public static boolean[] answers3 = new boolean[] {
             false, true, false, true, false, false, true, true
     };
 
-    //Antworten zu Runde4 real mit Hintergrund
+    //Antworten zu Runde4 reale Elemente mit Hintergrund
     // 1 1 0 1 0 1 0 0
     public static boolean[] answers4 = new boolean[] {
             true, true, false, true, false, true, false, false
     };
 
-    //Antworten zu Runde5 fiktiv ohne Hintergrund
+    //Antworten zu Runde5 fiktive Elemente ohne Hintergrund
     // 0 1 1 0 0 1 0 1
     public static boolean[] answers5 = new boolean[] {
             false, true, true, false, false, true, false, true
     };
 
-    //Antworten zu Runde6 fiktiv ohne Hintergrund
+    //Antworten zu Runde6 fiktive Elemente ohne Hintergrund
     // 1 0 1 0 1 0 1 0
     public static boolean[] answers6 = new boolean[] {
             true, false, true, false, true, false, true, false
     };
 
-    //Antworten zu Runde7 fiktiv ohne Hintergrund
+    //Antworten zu Runde7 fiktive Elemente ohne Hintergrund
     // 0 0 1 0 1 1 0 1
     public static boolean[] answers7 = new boolean[] {
             false, false, true, false, true, true,  false, true
     };
 
-    //Antworten zu Runde8 fiktiv ohne Hintergrund
+    //Antworten zu Runde8 fiktive Elemente ohne Hintergrund
     // 1 0 0 1 0 0 1 1
     public static boolean[] answers8 = new boolean[] {
             true, false, false, true, false, false, true, true
     };
 
-
-    //Antworten zu SetTraining Runde1 real mit Hintergrund
+    // Antworten für den TRAININGS MODUS
+    //Antworten zu SetTraining Runde1 reale Elemente mit Hintergrund
     // 0 1 0 1
     public static boolean[] training1answers = new boolean[] {
             false, true, false, true
     };
 
-    //Antworten zu SetTraining Runde2 fiktiv ohne Hintergrund
+    //Antworten zu SetTraining Runde2 fiktive Elemente ohne Hintergrund
     // 1 0 0 1
     public static boolean[] training2answers = new boolean[] {
             true, false, false, true
